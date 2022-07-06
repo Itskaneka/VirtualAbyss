e7d3291d1775438fe43d1544e2a0c42e84e83926
Itska <yuri.gomes.melo@gmail.com>
2022-07-06 03:20:07 -0300

    projeto de realidade virtual
    TCC SENAI

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public string nomeDaCena;
+    //Nome da Cena que escolhera no aplicativo unity e fara com que o botão puxe tal Cena
+    public void ChangeS()
+    {
+        SceneManager.LoadScene(nomeDaCena);
+    }
+    //Método que fara puxar a próxima tela do aplicativo.
+    public void Sair()
+    {
+        Application.Quit();
+    }
+    //Método que faz aplicativo fechar e finalizar no android.
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float velocidade = 3.5f;
+ 
+    
+    private float gravidade = 10f;
+   
+    private CharacterController controle;
+    //Variaveis aonde vão definir a velocidade e a gravidade em que o usuario ficara dentro da Realidade Virtual.
+    //Instanciando uma classe do Unity "CharacterController" para já o unity reconhecer o que estamos fazendo.
+    void Start()
+    {
+        controle = GetComponent<CharacterController>();
+        //Controle agora esta usando o componente da Classe CharacterController
+    }
+
+    
+    void Update()
+    {
+        
+        Movimentacao();
+        //atualiza a movimentação do jogador por frames 
+    }
+
+    void Movimentacao()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        //Pegando direção que a pessoa esta indo e enviando a informação para que o jogo entenda pra onde você esta querendo ir.
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+        Vector3 velocity = direction * velocidade;
+        velocity = Camera.main.transform.TransformDirection(velocity);
+        velocity.y -= gravidade;
+        controle.Move(velocity * Time.deltaTime);
+
+
+    }
+}
